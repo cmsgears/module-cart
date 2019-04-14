0673c6b2f307534163da7c92e6c44dd195bbc352
india <lokendra.chouhan@vulpinecode.com>
2019-04-14 19:29:05 +0530

    updated code

--- common/models/resources/CartItem.php
+++ common/models/resources/CartItem.php
@@ -19,15 +19,13 @@ use cmsgears\core\common\config\CoreGlobal;
 use cmsgears\cart\common\config\CartGlobal;
 
 use cmsgears\core\common\models\interfaces\base\IAuthor;
-use cmsgears\core\common\models\interfaces\base\IModelResource;
+
 use cmsgears\core\common\models\interfaces\resources\IGridCache;
 
-use cmsgears\core\common\models\base\Entity;
 use cmsgears\cart\common\models\base\CartTables;
 use cmsgears\cart\common\models\resources\Uom;
 
 use cmsgears\core\common\models\traits\base\AuthorTrait;
-use cmsgears\core\common\models\traits\base\ModelResourceTrait;
 use cmsgears\core\common\models\traits\resources\GridCacheTrait;
 
 use cmsgears\core\common\behaviors\AuthorBehavior;
@@ -74,7 +72,7 @@ use cmsgears\core\common\behaviors\AuthorBehavior;
  *
  * @since 1.0.0
  */
-class CartItem extends Entity implements IAuthor, IModelResource, IGridCache {
+class CartItem extends \cmsgears\core\common\models\base\ModelResource implements IAuthor, IGridCache {
 
 	// Variables ---------------------------------------------------
 
@@ -100,7 +98,6 @@ class CartItem extends Entity implements IAuthor, IModelResource, IGridCache {
 
 	use AuthorTrait;
 	use GridCacheTrait;
-	use ModelResourceTrait;
 
 	// Constructor and Initialisation ------------------------------
 
